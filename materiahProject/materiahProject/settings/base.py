b0b2598459af0d6783d3658f1c05ed0866a5b957
Anatomyah <anatomyah@protonmail.com>
2023-11-07 11:46:01 +0200

    7.11.23

--- materiahProject/materiahProject/settings/base.py
+++ materiahProject/materiahProject/settings/base.py
@@ -64,8 +64,8 @@ AWS_STORAGE_BUCKET_NAME = 'materiah1'
 AWS_S3_REGION_NAME = 'eu-central-1'
 AWS_DEFAULT_ACL = 'public-read'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/django-static/'
--- materiahProject/todo.py
+++ materiahProject/todo.py
@@ -1,3 +1,2 @@
 # todo - adjust admin console ro reflect Quote and Order changes
 # todo - django-crontab package
-# todo - check erro when creating order for qutoe 38
