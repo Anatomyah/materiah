33e28db1835dfc8296fd4f0284b6f3f88c25da72
Anatomyah <anatomyah@protonmail.com>
2023-11-07 14:50:31 +0200

    7.11.23

--- materiahProject/materiahProject/settings/base.py
+++ materiahProject/materiahProject/settings/base.py
@@ -145,5 +145,6 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 # Cron tasks
 CRONJOBS = [
     ('0 0 * * *', 'materiah.cronjobs.refresh_order_notifications'),
-    ('* * * * *', 'materiah.cronjobs.delete_failed_upload_statuses'),
+    ('* * * * *', 'materiah.cronjobs.delete_failed_upload_statuses',
+     '>> /var/www/materiah/materiahProject/logs/cronlog.log 2>&1'),
 ]
