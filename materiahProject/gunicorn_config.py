7ed592f48771ac0505fbb1b4fd0b5e308db204d7
Anatomyah <anatomyah@protonmail.com>
2023-11-17 06:59:38 +0200

    15.11.23

--- materiahProject/gunicorn_config.py
+++ materiahProject/gunicorn_config.py
@@ -1,7 +1,7 @@
 wsgi_app = "materiahProject.wsgi:application"
 bind = "0.0.0.0:8000"
 reload = True
-workers = 2
+workers = 3
 pidfile = "/var/www/materiah/materiahProject/runtime/gunicorn.pid"
 errorlog = '/var/www/materiah/materiahProject/logs/gunicorn-error.log'
 accesslog = '/var/www/materiah/materiahProject/logs/gunicorn-access.log'
